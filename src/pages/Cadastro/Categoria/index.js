dbd500907759dac08239c8f523919c4660a6dc97
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2020-07-30 11:55:55 -0300

    requisição para api json-server

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import { Button } from '../../../components/Button';
 
 const CadastroCategoria = () => {
   const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '',
-  }
+  };
 
   const [categorias, setCategorias] = useState([]);
   const [campos, setCampos] = useState(valoresIniciais);
+
+  const URL = 'http://localhost:8080/categorias';
+  useEffect(() => {
+    fetch(URL)
+      .then(async (resposta) => {
+        const respostaJson = await resposta.json();
+
+        setCategorias([...respostaJson]);
+      });
+  }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -23,7 +34,7 @@ const CadastroCategoria = () => {
 
     setCampos(valoresIniciais);
   }
-  
+
   function setValor(chave, valor) {
     setCampos({
       ...campos,
@@ -37,12 +48,15 @@ const CadastroCategoria = () => {
 
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {campos.nome}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {campos.nome}
+      </h1>
 
       <form onSubmit={handleSubmit}>
 
         <FormField
-          label="Nome da Categoria:"
+          label="Nome da Categoria"
           type="text"
           name="nome"
           value={campos.nome}
@@ -50,7 +64,7 @@ const CadastroCategoria = () => {
         />
 
         <FormField
-          label="Descrição da Categoria:"
+          label="Descrição da Categoria"
           type="textarea"
           name="descricao"
           value={campos.descricao}
@@ -58,23 +72,23 @@ const CadastroCategoria = () => {
         />
 
         <FormField
-          label="Cor da Categoria:"
+          label="Cor"
           type="color"
           name="cor"
           value={campos.cor}
           onChange={handleChange}
         />
 
-        <button type="submit">
+        <Button type="submit">
           Cadastrar
-        </button>
+        </Button>
 
       </form>
 
       <ul>
-        {categorias.map((categoria, index) => (
-          <li key={index}>
-            {categoria.nome}
+        {categorias.map((categoria) => (
+          <li key={categoria.id}>
+            {categoria.titulo}
           </li>
         ))}
       </ul>
